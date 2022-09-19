f8394c22786b593925d73297478f374548e6f277
Dev Dan <danboyle8637@gmail.com>
2022-09-19 19:05:52 -0400

    Building the blog call to action sections.

---
+++ src/data/callToActions.ts
@@ -0,0 +1,43 @@
+import type { CopyContentService } from "../types/components";
+
+export const copyAndContentServices: CopyContentService[] = [
+  {
+    id: 0,
+    imageUrl:
+      "https://ik.imagekit.io/csu76xuqqlwj/nerds-who-sell/site-assets/web-design-dev-card-icon_xz0189uue.png?ik-sdk-version=javascript-1.4.3",
+    altTag: "Website design and development.",
+    titleTag: "Website Design and Development",
+    headline: "Web Design & Dev",
+    description:
+      "Get sales copy for the money pages on your website so they <strong>convert leads and make sales</strong>.",
+    buttonLabel: "I Need a New Site",
+    buttonTitle: "Get a New Site Today",
+    slug: "/services/web-design-and-development",
+  },
+  {
+    id: 1,
+    imageUrl:
+      "https://ik.imagekit.io/csu76xuqqlwj/nerds-who-sell/site-assets/seven-day-site-card-icon_eyDc3K932.png?ik-sdk-version=javascript-1.4.3",
+    altTag: "7 Day Site Design.",
+    titleTag: "7 Day Site Design",
+    headline: "7 Day Site",
+    description:
+      "Get sales copy for the money pages on your website so they <strong>convert leads and make sales</strong>.",
+    buttonLabel: "I Want a 7 Day Site",
+    buttonTitle: "Get a 7 Day Site",
+    slug: "/services/seven-day-site",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://ik.imagekit.io/csu76xuqqlwj/nerds-who-sell/site-assets/copywriting-card-icon_eJi4X0qw6.png?ik-sdk-version=javascript-1.4.3",
+    altTag: "Copywriting serives.",
+    titleTag: "Copywriting Services To Sell",
+    headline: "Web Design & Dev",
+    description:
+      "Get sales copy for the money pages on your website so they <strong>convert leads and make sales</strong>.",
+    buttonLabel: "I Need a New Site",
+    buttonTitle: "Get a New Site Today",
+    slug: "/services/web-design-and-development",
+  },
+];
