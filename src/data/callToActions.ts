f2bd040b9ec55adeb082a8ff39d7a9c931cc1b74
Dev Dan <danboyle8637@gmail.com>
2022-09-20 07:50:31 -0400

    Still working on the about page and changing design as I build. Never good.

--- src/data/callToActions.ts
+++ src/data/callToActions.ts
@@ -4,7 +4,7 @@ export const copyAndContentServices: CopyContentService[] = [
   {
     id: 0,
     imageUrl:
-      "https://ik.imagekit.io/csu76xuqqlwj/nerds-who-sell/site-assets/web-design-dev-card-icon_xz0189uue.png?ik-sdk-version=javascript-1.4.3",
+      "https://ik.imagekit.io/csu76xuqqlwj/nerds-who-sell/site-assets/web-design-dev-card-icon_9si6hjKZ5.png?ik-sdk-version=javascript-1.4.3",
     altTag: "Website design and development.",
     titleTag: "Website Design and Development",
     headline: "Web Design & Dev",
@@ -17,7 +17,7 @@ export const copyAndContentServices: CopyContentService[] = [
   {
     id: 1,
     imageUrl:
-      "https://ik.imagekit.io/csu76xuqqlwj/nerds-who-sell/site-assets/seven-day-site-card-icon_eyDc3K932.png?ik-sdk-version=javascript-1.4.3",
+      "https://ik.imagekit.io/csu76xuqqlwj/nerds-who-sell/site-assets/seven-day-site-card-icon_mL4jeBHqh.png?ik-sdk-version=javascript-1.4.3",
     altTag: "7 Day Site Design.",
     titleTag: "7 Day Site Design",
     headline: "7 Day Site",
@@ -30,14 +30,27 @@ export const copyAndContentServices: CopyContentService[] = [
   {
     id: 2,
     imageUrl:
-      "https://ik.imagekit.io/csu76xuqqlwj/nerds-who-sell/site-assets/copywriting-card-icon_eJi4X0qw6.png?ik-sdk-version=javascript-1.4.3",
+      "https://ik.imagekit.io/csu76xuqqlwj/nerds-who-sell/site-assets/copywriting-card-icon_ZI5CqWSll.png?ik-sdk-version=javascript-1.4.3",
     altTag: "Copywriting serives.",
     titleTag: "Copywriting Services To Sell",
-    headline: "Web Design & Dev",
+    headline: "Copywriting Services",
     description:
       "Get sales copy for the money pages on your website so they <strong>convert leads and make sales</strong>.",
     buttonLabel: "I Need a New Site",
     buttonTitle: "Get a New Site Today",
+    slug: "/services/web-design-and-development",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://ik.imagekit.io/csu76xuqqlwj/nerds-who-sell/site-assets/marketing-nerd-chat-icon_oLnwTngON.png?ik-sdk-version=javascript-1.4.3",
+    altTag: "Marketing chat bubbles",
+    titleTag: "Marketing Nerd Chat",
+    headline: "Marketing Nerd Chat",
+    description:
+      "Get sales copy for the money pages on your website so they <strong>convert leads and make sales</strong>.",
+    buttonLabel: "Let's Talk Leads & Sales",
+    buttonTitle: "Let's Talk Leads & Sales",
     slug: "/services/web-design-and-development",
   },
 ];
