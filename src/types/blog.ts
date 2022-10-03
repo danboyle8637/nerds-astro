af329c861ea13bbbf82751037cfa98f9819a40de
Dev Dan <danboyle8637@gmail.com>
2022-10-03 19:33:53 -0400

    Building the blog cards and getting them working for the now part of the site. Then the blog parts.

---
+++ src/types/blog.ts
@@ -0,0 +1,23 @@
+type AuthorName = "Dan" | "Kindal";
+
+type BlogCategory = "Now" | "Design" | "Coding" | "Marketing" | "Tech";
+
+export interface Author {}
+
+export interface BlogPostFrontmatter {
+  draft: boolean;
+  date: string;
+  seoTitle: string;
+  seoDescription: string;
+  headline: string;
+  cardTeaser: string;
+  author: AuthorName;
+  featureImageUrl: string;
+  socialImageUrl: string;
+  altTag: string;
+  titleTag: string;
+  buttonLabel: string;
+  tags: string[];
+  category: BlogCategory;
+  slug: string;
+}
