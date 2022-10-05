58e67d33b0303af05aa1681b305b789b1685ccdd
Dev Dan <danboyle8637@gmail.com>
2022-10-05 07:29:55 -0400

    Building the post components so I can just throw them into the serializer and render my posts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly SANITY_PROJECT_ID: string;
+  readonly SANITY_BASE: string;
+  readonly SANITY_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
