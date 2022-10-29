ad834e5209894902b5d313020c283e7fbbeaa580
Dev Dan <danboyle8637@gmail.com>
2022-10-29 08:02:59 -0400

    Getting rid of cloudflare config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), mdx()],
-  output: "server",
-  adapter: cloudflare({ mode: "directory" }),
 });
